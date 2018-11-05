26f22145a26ef6a2699763639fd2e5f386a55901
hoodielive <hoodielive@gmail.com>
2018-11-05 16:14:56 -0500

    Joi will take over validating input

---
+++ archive/building_restful_apis/express-demo/app.js
@@ -0,0 +1,49 @@
+/** DEPENDENCIES **/ 
+const express = require('express');
+const app = express(); 
+const Joi = require('joi');  
+
+/** VARIABLES **/
+const courses = [
+  { id: 1, name: 'Course 1'},
+  { id: 2, name: 'Course 2'},
+  { id: 3, name: 'Course 3'},
+];
+
+/** GET REQUEST **/
+app.get('/api/courses', (req, res) => {
+  res.send(courses); 
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = (c => c.id === req.params.id); 
+  if (!course) res.status(404).send('Sorry, you have requested a resource that does NOT exist.');
+  res.send(course)
+});
+
+/** POST **/ 
+app.post('/api/courses', (req, res) => {
+
+  /** if (!req.body.name || req.body.name.length < 3 ) {
+    // 400 Bad Request 
+    res.status(400).send('Sorry, you must provide a name greater than 3 characters.')
+   };**/
+
+  /** JOI Validation **/
+  const schema = {
+  name: Joi.string().min(3).required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+
+  const course = {
+    id: courses.length + 1, 
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+}); 
+
+/** SERVER STUFF **/
+const port = process.env.PORT || 3000; 
+app.listen(port, () => console.log(`Listening on port ${port}...`)); 
