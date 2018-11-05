6aa07b423cfb59de80b84a0a8297913dec9ad49c
hoodielive <hoodielive@gmail.com>
2018-11-05 13:45:55 -0500

    apitis

--- archive/building_restful_apis/express-demo/index.js
+++ archive/building_restful_apis/express-demo/index.js
@@ -3,12 +3,20 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Howdy Aeon!')
-
 }); 
 
 app.get('/api/courses', (req, res) => {
   res.send([1,2,3]);
 }); 
 
-port = 3000;
+// /api/courses 
+app.get('/api/courses/:id', (req, res) => {
+  res.send(req.params.id); 
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+  res.send(req.params); 
+});
+
+const port = process.env.PORT || 3000; 
 app.listen(port, () => console.log(`Listening on port ${port}`)); 
