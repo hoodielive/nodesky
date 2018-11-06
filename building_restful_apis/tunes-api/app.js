f733f817ee23443407f52db534b8fa5999bbae35
hoodielive <hoodielive@gmail.com>
2018-11-05 21:42:31 -0500

    my tune-api lol

--- building_restful_apis/tunes-api/app.js
+++ building_restful_apis/tunes-api/app.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 const express = require('express');
 const app = express(); 
 
-app.use(express.JSON());
+app.use(express.json());
 
 /** MUSIC VAR **/
 artists = [
@@ -25,4 +25,39 @@ app.get('/api/artists/:id', (req, res) => {
   res.send(artist)
 });
 
-function validateArtist()
+app.post('/api/artists/:id', (req, res) => {
+  const { error } = validateArtist(req.body); 
+  if (error) return res.sendStatus(400).send(error.details[0].message); 
+
+  const artist = {
+    id: artists.length + 1, 
+    name: req.body.name
+  }; 
+}); 
+
+app.put('/api/artists/:id', (req, res) => {
+  const artist = artists.find(a => a.id === parseInt(req.params.id)); 
+  if (!artist) return res.sendStatus(400).send('Are you cracked? This ID does not exist!')
+  const { error } = validateArtist(req.body); 
+  if (error ) return res.sendStatus(400).send(error.details[0].message); 
+  artist.name = req.body.name;
+  res.send(artist);
+});
+
+app.delete('/api/artists/:id', (req, res) => {
+  const artist = artists.find(a => a.id === parseInt(req.params.id)); 
+  if (!artist) return res.sendStatus(404).send('Artist does not exist!')
+  const index = artists.indexOf(artist); 
+  artists.splice(index, 1); 
+  res.send(artist)
+})
+function validateArtist(artist) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  return Joi.validate(artist, schema);
+}
+
+/** SERVER STUFF **/
+const port = process.env.PORT || 3000; 
+app.listen(port, () => console.log(`Listening on port ${port}...`)); 
