52d829205360eeb990ea780c1246725e96349721
hoodielive <hoodielive@gmail.com>
2018-11-08 18:49:26 -0500

    consuming the promises

--- async-demo/archive/callback_hell_replaced_with_promises.js
+++ async-demo/archive/callback_hell_replaced_with_promises.js
@@ -1,42 +1,40 @@
 console.log('Before');
 
-getUser(1, (user) => {
-  getRepositories(user.gitHubUsername, (repos) => {
-    getCommits(repo, (commits) => {
-      console.log(commits);
-    })
-  });
-});
+getUser(1)
+  .then(user => getRepositories(user.gitHubUsername)
+    .then(repos => getCommits(repo[0]))
+    .then(commits => console.log('Commits', commits))
+    .catch(err => console.log('Error', err.message));
 
-console.log('After');
+    console.log('After');
 
-function getUser(id) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Reading a user from a database....');
-      resolve({
-        id: id,
-        gitHubUsername: 'lawrence'
+    function getUser(id) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log('Reading a user from a database....');
+          resolve({
+            id: id,
+            gitHubUsername: 'lawrence'
+          });
+        }, 2000);
       });
-    }, 2000);
-  });
-};
+    };
 
-function getRepositories(username) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Connecting to the Github API')
-      resolve(['repo1', 'repo2', 'repo3']);
-    }, 2000);
-  });
-};
+    function getRepositories(username) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log('Connecting to the Github API')
+          resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+      });
+    };
 
 
-function getCommits(repo) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Calling the Github API');
-      resolve(['commit'])
-    }, 2000);
-  });
-};
+    function getCommits(repo) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log('Calling the Github API');
+          resolve(['commit'])
+        }, 2000);
+      });
+    };
