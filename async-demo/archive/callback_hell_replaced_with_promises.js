29b37ec4e613d3bace97fe478978dc4fcaed9f9c
hoodielive <hoodielive@gmail.com>
2018-11-08 17:42:11 -0500

    Callback hell transcended!

--- async-demo/archive/callback_hell_replaced_with_promises.js
+++ async-demo/archive/callback_hell_replaced_with_promises.js
@@ -10,16 +10,30 @@ getUser(1, (user) => {
 
 console.log('After');
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a user from a database....');  
-    callback({ id: id, gitHubUsername: 'lawrence'});
-  }, 2000); 
-}
+function getUser(id) {
+    return new Promise((resolve,reject) => {
+      setTimeout(() => {
+        console.log('Reading a user from a database....');  
+        resolve({ id: id, gitHubUsername: 'lawrence'});
+    }, 2000); 
+  });
+};
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log('Connecting to the Github API')
-    callback(['repo1', 'repo2', 'repo3']);
-  }, 2000);
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log('Connecting to the Github API')
+          resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  });
 }; 
+
+
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log('Calling the Github API');
+          resolve(['commit'])
+        }, 2000);
+    });
+};
