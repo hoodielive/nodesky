03fc15ef170a17edf2bfdd9525c386b7a5cda469
hoodielive <hoodielive@gmail.com>
2018-11-08 19:27:08 -0500

    players efff up..lol

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err); 
+  .catch(err => console.error('Could not connect to MongoDB...', err)); 
