c0fedc9006b06e6741130860068963b166f609b7
hoodielive <hoodielive@gmail.com>
2018-11-08 22:24:18 -0500

    mongod queries

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -9,15 +9,31 @@ const courseSchema = new mongoose.Schema({
   author: String, 
   tags: [ String ],
   date: { type: Date, default: Date.now }, 
-  isPublished: Boolean
+  isPublished: Boolean,
 });
 
-/** By the way: Schema types are:
- * String
- * Number
- * Date
- * Buffer
- * Boolean
- * ObjectID
- * Array
-**/ 
+async function createCourse() {
+  const Course = mongoose.model('Course', courseSchema); 
+  const course = new Course({
+    name: 'React Course', 
+    author: 'Larry', 
+    tags: ['react', 'frontend'],
+    isPublished: true
+  });
+
+  const result = await course.save();
+  console.log(result);
+};
+
+async function getCourses() {
+  const Course = mongoose.model('Course', courseSchema); 
+  const courses = await Course
+  //.find({ author: 'Larry', isPublished: true })
+    .find({ price: { $gte: 10, $lte: 20 } })
+    .limit(10)
+    .sort({ name: -1 })
+    .select( { name: 1, tags: 1 })
+  console.log(courses); 
+}; 
+
+getCourses(); 
