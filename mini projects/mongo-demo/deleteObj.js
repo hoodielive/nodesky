ad21cda3fc2b2674ea20b7d1a6dbddae716f0829
hoodielive <hoodielive@gmail.com>
2018-11-10 16:18:07 -0500

    Mongo Crud Operations

--- mini projects/mongo-demo/deleteObj.js
+++ mini projects/mongo-demo/deleteObj.js
@@ -1,5 +1,7 @@
 async function removeCourse(id) {
  const result = await Course.deleteOne({ _id: id});
+  // for many do deleteMany({}) -> returns how many were deleted
+  // findByIdAndRemove(id); 
   console.log(result); 
 }
 
