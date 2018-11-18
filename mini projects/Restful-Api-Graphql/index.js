1b62c089eebee81b697f97de341fd89a555cd6c4
hoodielive <hoodielive@gmail.com>
2018-11-18 14:52:55 -0500

    using multiple routes

---
+++ mini projects/Restful-Api-Graphql/index.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+
+const app = express(); 
+
+const apiRouter = express.Router(); 
+
+apiRouter.get('/', (req, res) => res.json({ apiRouter: true }))
+
+app.use('/', apiRouter); 
+
+app.get('/', (req, res) => {
+  res.json({ first: true }); 
+}); 
+
+
+const port = process.env.PORT || 3000; 
+app.listen(port, () =>console.log(`Listening on port ${port}`));  
